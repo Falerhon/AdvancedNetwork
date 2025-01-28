19a81d80ad7882e8bed0825f9dce2b748417f92e
Falerhon <alex-lex360@hotmail.com>
2025-01-28 17:24:15 -0500

    feat : client now asks the type of connection

--- TP1/Client/main.cpp
+++ TP1/Client/main.cpp
@@ -13,8 +13,31 @@ std::string TypeMessage() {
 int main() {
     Socket* socket = Socket::CreateSocket();
 
-    //Change the string here to change the destination IP
-    const std::string IPAddress = "127.0.0.1";
+    std::string IPAddress;
+    std::string AddressType;
+
+    std::cout << "Type of destination address : IPV4 (4) or IPV6 (6)";
+
+    while (true) {
+        std::cin >> AddressType;
+
+        //String comparison is bad
+        if (AddressType == "4") {
+            IPAddress = "127.0.0.1";
+            break;
+        } else if (AddressType == "6") {
+            IPAddress = "::1";
+            break;
+        } else {
+            std::cin.clear();
+            std::cout << "Input invalid. Valid inputs : 4 - 6";
+        }
+    }
+
+    //Clear the cin
+    std::cin.clear();
+    std::cin.ignore();
+
     socket->CreateClientSocket(IPAddress);
 
     socket->MessageSend(TypeMessage());
