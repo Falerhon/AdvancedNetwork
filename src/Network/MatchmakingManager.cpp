0664131d830afde59bc4f0ccdb0b534caed0ba99
Falerhon <alex-lex360@hotmail.com>
2025-04-27 16:15:59 -0400

    Looking for session can now find a session

--- src/Network/MatchmakingManager.cpp
+++ src/Network/MatchmakingManager.cpp
@@ -4,6 +4,8 @@
 
 #include "MatchmakingManager.h"
 
+#include <iostream>
+
 #include "Corrade/Utility/Debug.h"
 
 void MatchmakingManager::update() {
@@ -22,6 +24,23 @@ void MatchmakingManager::checkMatchmakingStatus() {
     auto response = API->post("/api/matchmaking/Status", payload);
     if (response.status_code == 200) {
         Corrade::Utility::Debug{} << "Matchmaking request successful";
+
+        std::string serverAddress;
+
+        try {
+            auto json = nlohmann::json::parse(response.text);
+            serverAddress = std::string(json.at("ip")) + ":" + std::to_string(int(json.at("port")));
+            std::cout << "Server found : " << serverAddress;
+
+            //TODO : Actual server connection
+            if (serverAddress.size() > 1) {
+
+            }
+
+        } catch (const std::exception& e) {
+            std::cerr << "Still looking : " << e.what() << std::endl;
+        }
+
     } else {
         Corrade::Utility::Error{} << "Failed to check matchmaking : " << response.status_code;
     }
